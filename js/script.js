ec123d2add7ea5f4e661f307eb8eceb93aa02907
HlebNoGleb <40634698+HlebNoGleb@users.noreply.github.com>
2019-09-04 21:15:46 +0300

    create

---
+++ js/script.js
@@ -0,0 +1,11 @@
+$('.slider').slick({
+  infinite: true,
+  slidesToShow: 1,
+  dots: true,
+});
+var colors = ["red", "orange", "yellow", "green", "blue"];
+var currentIndex = 0;
+
+$(".slider").on("afterChange", function (){
+    $(".wrapper").css("background-color", colors[currentIndex++%colors.length]);
+});
