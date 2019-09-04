990c4fbcc08b5c62c41f78dc9295bf5ead2c5707
HlebNoGleb <40634698+HlebNoGleb@users.noreply.github.com>
2019-09-04 21:24:00 +0300

    1

--- js/script.js
+++ js/script.js
@@ -3,7 +3,7 @@ $('.slider').slick({
   slidesToShow: 1,
   dots: true,
 });
-var colors = ["red", "orange", "yellow", "green", "blue"];
+var colors = ["red", "orange", "pink", "green", "blue"];
 var currentIndex = 0;
 
 $(".slider").on("afterChange", function (){
